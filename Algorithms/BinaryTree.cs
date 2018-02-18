11d7baba403d64689766233a02cc1f66af877bb9
wit20ger33grineta24xaradas77 <vaultec114@gmail.com>
2018-02-18 16:12:49 +0200

    added binary tree and c# 7.0 new features

--- Algorithms/BinaryTree.cs
+++ Algorithms/BinaryTree.cs
@@ -4,49 +4,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Train
+namespace TrainCode
 {
+
     class Node
     {
         public int data;
-        public Node right;
         public Node left;
-        public Node(int val)
+        public Node right;
+
+        public Node(int data)
         {
-            this.data = val;
+            this.data = data;
         }
-        
-        
+
+        public void AddNode(Node node)
+        {
+            if (this.data > node.data)
+            {
+                if (this.left == null)
+                {
+                    this.left = node;
+                }
+                else
+                {
+                    this.left.AddNode(node);
+                }
+            }
+            if (this.data < node.data)
+            {
+                if (this.right == null)
+                {
+                    this.right = node;
+                }
+                else
+                {
+                    this.right.AddNode(node);
+                }
+            }
+        }
+        public void Traverse()
+        {
+            if (this.left != null)
+                this.left.Traverse();
+
+            Console.WriteLine(this.data);
+
+            if (this.right != null)
+                this.right.Traverse();
+        }
     }
-    
+
+
+    class Tree
+    {
+        public Node root;
+
+        public void AddValue(int value)
+        {
+            Node node = new Node(value);
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                root.AddNode(node);
+            }
+        }
+        public void Print()
+        {
+            this.root.Traverse();
+        }
+
+    }
 
     class Program
     {
+        public static Random r;
         static void Main(string[] args)
         {
+            r = new Random();
+            Tree tree = new Tree();
+            tree.AddValue(50);
+            for (int i = 1; i <= 99;i++)
+            {
+                tree.AddValue(r.Next(1, 100));
+            }
 
-            Node root;
-            Tree(root, 1, 10);
-            Console.WriteLine("azaza");
-        }
+            tree.Print();
 
-        public static void Tree(Node node,int num,int count)
-        {
-            if (count <= 0)
-                return;
-
-            if (node == null)
-                node = new Node(num);
-
-            num++;
-            node = new Node(num);
-            Tree(node.left, num, count);
-
-            num++;
-            node = new Node(num);
-            Tree(node.right, num, count);
-        }
-
-      
+            Console.WriteLine("Done");
+        }   
     }
 }
---
+++ Examples/c#7.0.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainCode
+{
+
+    class Employee
+    {
+        public int Id { get; set; } = 1001;
+        public string FirstName { get; set; } = "John";
+        public string[] GetCountries() => new string[] { "Canada", "USA" };
+    }
+    class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name ?? throw new ArgumentNullException(name);
+        public string GetFirstName()
+        {
+            //var parts = Name.Split(" ");
+            return (parts.Length > 0) ? parts[0] : throw new InvalidOperationException("No name!");
+        }
+        public string GetLastName() => throw new NotImplementedException();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            string str = $"Emloyee name is {emp.FirstName}, id: {emp.Id}";
+            string name = emp?.FirstName;
+            var countries = new Dictionary<int, string> { [0] = "USA",
+                [1] = "England" };
+            var result = GetDivisionResults(17, 5);
+            Console.WriteLine(result.Item1+" "+ result.Item2);
+        }
+
+        static (int, int) GetDivisionResults(int number, int divisor)
+        {
+
+            int quotient = number / divisor;
+            int remainder = number % divisor;
+            return (quotient, remainder);
+        }
+
+
+
+    }
+}
